69b7d922f961c325cf9914ce2f9cba52d79980bf
Alessandro Camilli <a.camilli@yahoo.it>
2016-01-18 17:04:55 +0100

    version updated

--- l10n_it_assets/__openerp__.py
+++ l10n_it_assets/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Italian Assets Management',
-    'version': '8.0.1.1',
+    'version': '8.0.1.2',
     'category': 'Localization/Italy',
     'description': """
         - Parallel Depreciation Board for fiscal values
