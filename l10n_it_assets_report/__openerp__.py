91ed1ea3711b84db5abda616b9708290bc2d87f1
alessandrocamilli <alessandrocamilli@openforce.it>
2016-02-10 03:07:35 -0500

    Updated assets version

--- l10n_it_assets_report/__openerp__.py
+++ l10n_it_assets_report/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Italian Assets Management Report',
-    'version': '1.1.6',
+    'version': '8.0.1',
     'category': 'Localization/Italy',
     'summary': 'Italian Assets Management',
     'author': 'Openforce di Alessandro Camilli, \
