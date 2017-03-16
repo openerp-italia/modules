b68499f01d83663d8288cabf09016c04312b3187
eLBati <lorenzo.battistini@agilebg.com>
2017-03-16 10:12:42 +0100

    REF account_fiscal_agent_intrastat: calcola intrastat solo per la fattura del rappresentante fiscale, se impostato nella sua posizione fiscale rename to account_invoice_fiscal_agent_intrastat REF action_move_create to avoid multiple calls

---
+++ account_invoice_fiscal_agent_intrastat/README.rst
@@ -0,0 +1,25 @@
+.. image:: https://img.shields.io/badge/licence-LGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/lgpl-3.0-standalone.html
+   :alt: License: LGPL-3
+
+==============================
+Account Fiscal Agent Intrastat
+==============================
+
+This module extends the account_invoice_fiscal_agent module adding to it the
+functionality of calculating the instrastat lines on the fiscal agent invoice
+
+Configuration
+=============
+
+account_invoice_fiscal_agent module can be found `here
+<https://github.com/OCA/account-invoicing>`_
+
+Credits
+=======
+
+Contributors
+------------
+
+* Alex Comba <alex.comba@agilebg.com>
+* Lorenzo Battistini <lorenzo.battistini@agilebg.com>
--- account_invoice_fiscal_agent_intrastat/__openerp__.py
+++ account_invoice_fiscal_agent_intrastat/__openerp__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Alex Comba - Agile Business Group
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2017 Lorenzo Battistini - Agile Business Group
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Account Fiscal Agent Intrastat',
+    'name': 'Account Fiscal Agent - Intrastat',
     'version': '8.0.1.0.0',
     'category': 'Accounting & Finance',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'author': 'Agile Business Group',
-    'license': 'AGPL-3',
     'website': 'https://www.agilebg.com/',
     'depends': [
-        'account_fiscal_agent',
+        'account_invoice_fiscal_agent',
         'l10n_it_intrastat',
     ],
     'installable': True,
