3055342a0a099287dcb103a6f885754a7dd6c9c9
eLBati <lorenzo.battistini@agilebg.com>
2017-09-28 12:52:22 +0200

    ADD l10n_it_comunicazione_dati_iva copied from 8.0

---
+++ l10n_it_comunicazione_dati_iva/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2017 Alessandro Camilli - Openforce
+# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl).
+
+{
+    'name': 'Comunicazione dati IVA',
+    'description': 'Gestione Comunicazione dati IVA ed export file '
+                   'xml conforme alle specifiche dell''Agenzia delle Entrate',
+    'version': '8.0.1.0.0',
+    'category': 'Account',
+    'author': "Openforce di Camilli Alessandro",
+    'website': 'http://www.odoo-italia.net',
+    'license': 'GPL-3',
+    'depends': [
+        'account', 'l10n_it_fiscal_document_type', 'l10n_it_codici_carica',
+        'l10n_it_fiscalcode', 'l10n_it_esigibilita_iva',
+        'l10n_it_account_tax_kind', 'account_invoice_entry_date'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/compute_fiscal_document_type_view.xml',
+        'views/comunicazione.xml',
+        'views/account.xml',
+        'views/account_invoice_view.xml',
+        'wizard/export_file_view.xml',
+    ],
+    'installable': True,
+}
